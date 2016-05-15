d911895eee19f4696f2d522935ea86ee74cd28a2
zhoujingyu <l>
2016-05-15 15:26:58 +0800

    完成直播间所有功能，接下来去做用户界面

--- live/js/live.js
+++ live/js/live.js
@@ -56,7 +56,11 @@ function showDataBox(){
 //取得赛程
 function getSchedule(){
 	var date=getDateString();
-	var sql="select * from schedule where schedule_date>='"+date+"' order by schedule_date";
+	var sql="select * "+
+			"from schedule "+
+			"where schedule_date>='"+date+"' "+
+			"and liver_id="+liver_id+" "+
+			"order by schedule_date";
 	$.ajax({
 		type:'GET',
 		url:"../../php/hasReturnQuery.php",
@@ -65,7 +69,8 @@ function getSchedule(){
 		},
 		dataType:'json',
 		success:function(data){
-			$('.rightBox .scheduleList').remove();
+			var $rightBox=$('#rightBox');
+			$rightBox.find('.scheduleList').remove();
 			if (data != '') {
 				var scheduleListHTML='<div class="scheduleList borderBottom">'+
 		 								'<p class="name textLeft">rpname</p><!--'+
@@ -73,7 +78,7 @@ function getSchedule(){
 		 								'--><p class="datetime textLeft">rptime</p><!--'+
 		 								'--><p class="status textRight">rpstatus</p><!--'+
 		 								'--><p class="endGame textRight" style="width:100%;display:none">'+
-		 								'<button type="button" class="endButton">结束本场比赛</button>'+
+		 								'<button type="button" class="endButton" onclick="endThisGame(rpschedule_id2,\'rptable2\',this)">结束本场比赛</button>'+
 		 								'</p><!--'+
 		 							 '</div>';
 		 		var HTMLTmp='';
@@ -90,11 +95,15 @@ function getSchedule(){
 		 									.replace('rptime',data[i]['schedule_date']+','+data[i]['schedule_time'].substring(0,5))
 		 									.replace('rpstatus',data[i]['schedule_time'].substring(0,5))
 		 									.replace('rptable',table)
-		 									.replace('rpschedule_id',data[i]['schedule_id']);
-		 			$('.rightBox').append(HTMLTmp);	 			
+		 									.replace('rpschedule_id',data[i]['schedule_id'])
+		 									.replace('rptable2',table)
+		 									.replace('rpschedule_id2',data[i]['schedule_id']);
+		 			$rightBox.append(HTMLTmp);	 			
 		 			ms=getMs(data[i]['schedule_date'],data[i]['schedule_time']);
-		 			checkGameStartOrNot($('.rightBox .scheduleList:last .status'),table,ms);
+		 			checkGameStartOrNot($rightBox.find('.scheduleList:last .status'),table,ms);
 		 		}
+			}else{
+				$rightBox.append('<p class="scheduleList borderBottom" style="color:white">暂无直播,赶快叫管理人员添加吧!</p>');
 			}
 		},
 		error:function(){
@@ -105,7 +114,6 @@ function getSchedule(){
 //检查比赛是否开始
 function checkGameStartOrNot($p,table,ms){
 	var todayMs=getTodayMs();
-	// console.log(todayMs+','+ms);
 	if (todayMs < ms) {
 		var remainTime=getRemainTime(todayMs,ms);
 		$p.text(remainTime);
@@ -115,7 +123,7 @@ function checkGameStartOrNot($p,table,ms){
 		return;
 	}
 
-	var sql="select * from "+table+";";
+	var sql="select schedule_id,end_flag from "+table+";";
 	$.ajax({
 		type:'GET',
 		url:"../../php/hasReturnQuery.php",
@@ -124,15 +132,52 @@ function checkGameStartOrNot($p,table,ms){
 		},
 		dataType:'json',
 		success:function(data){
+			console.log('检查比赛是否开始数据↓');
 			console.log(data);
 			if (data != '') {
-				
+				switch(data[0]['end_flag']){
+					case '0':{
+						changeToPlaying($p,table,data[0]['schedule_id']);
+					}break;
+					case '1':{
+						$p.text('比赛中');
+					}break;
+					case '2':{
+						$p.text('已结束');
+						$p.siblings('.endGame').remove();
+					}break;
+					default:break;
+				}
 			}else{
 
 			}
 		},
 		error:function(){
 			console.log('连接后台错误'+table);
+		}
+	});
+}
+//时间到，进入直播状态
+function changeToPlaying($p,table,schedule_id){
+	console.log('时间到，进入直播状态');
+	var sql="update "+table+" "+
+			"set end_flag=1 "+
+			"where schedule_id="+schedule_id+";";
+	$.ajax({
+		type:'GET',
+		url:"../../php/noReturnQuery.php",
+		data:{
+			sql:sql
+		},
+		success:function(data){
+			if (data == 1) {
+				$p.text('比赛中');
+			}else{
+				$p.text('已开始,未开启');
+			}
+		},
+		error:function(){
+			console.log('已开始,未开启')
 		}
 	});
 }
@@ -198,46 +243,105 @@ function initLivingroom(schedule_id,table,p){
 		dataType:'json',
 		success:function(data){
 			if (data != '') {
-				var sqlInsert="insert into "+table+" "+
-							  "values "+
-							  "("+
-							  data[0]['schedule_id']+","+
-							  data[0]['guest_id']+","+
-							  "'"+data[0]['guest_name']+"',"+
-							  data[0]['home_id']+","+
-							  "'"+data[0]['home_name']+"',"+
-							  data[0]['liver_id']+","+
-							  "'"+data[0]['liver_name']+"',"+
-							  "1,0,0,0,0,0,0,0,0,0,0,"+
-							  "'12:00',0,0"
-							  +");";
-				$.ajax({
-					type:'GET',
-					url:"../../php/noReturnQuery.php",
-					data:{
-						sql:sqlInsert
-					},
-					success:function(){
-						enterLiving(schedule_id,table,p);
-					},
-					error:function(){
-						console.log('初始化插入数据失败');
-					}
-				});
-
-				//初始化球员的数据
-				var guest_id=data[0]['guest_id'];
-				var home_id=data[0]['home_id'];
-				var sqlPlayer="select player_id "+
-							  "from player "+
-							  "where team_id="+guest_id+" "+
-							  "or team_id="+home_id+";";
+				console.log('取得赛程数据成功，即将初始化直播间表数据');
+				initLivingroomData(schedule_id,table,p,data);
 			}
 		},
 		error:function(){
 			console.log('连接后台错误'+table);
 		}
 	});
+}
+//初始化直播间表数据
+function initLivingroomData(schedule_id,table,p,data){
+	var sqlInsert="insert into "+table+" "+
+				  "values "+
+				  "("+
+				  data[0]['schedule_id']+","+
+				  data[0]['guest_id']+","+
+				  "'"+data[0]['guest_name']+"',"+
+				  data[0]['home_id']+","+
+				  "'"+data[0]['home_name']+"',"+
+				  data[0]['liver_id']+","+
+				  "'"+data[0]['liver_name']+"',"+
+				  "1,0,0,0,0,0,0,0,0,0,0,"+
+				  "'12:00',0,0"+
+				  ");";
+	$.ajax({
+		type:'GET',
+		url:"../../php/noReturnQuery.php",
+		data:{
+			sql:sqlInsert
+		},
+		success:function(){
+			console.log('初始化直播间表数据成功，即将取得球员数据');
+			initPlayerData(schedule_id,table,p,data);
+		},
+		error:function(){
+			console.log('初始化直播间表数据失败');
+		}
+	});
+}
+//初始化该比赛球员数据
+function initPlayerData(schedule_id,table,p,data){
+	var guest_id=data[0]['guest_id'];
+	var home_id=data[0]['home_id'];
+	var sqlPlayer="select player_id,team_id "+
+				  "from player "+
+				  "where team_id="+guest_id+" "+
+				  "or team_id="+home_id+";";
+	$.ajax({
+		type:'GET',
+		url:"../../php/hasReturnQuery.php",
+		data:{
+			sql:sqlPlayer
+		},
+		dataType:'json',
+		success:function(playerData){
+			if(playerData != ''){
+				console.log('取得球员数据成功，即将初始化插入球员数据');
+				initInsertPlayerData(schedule_id,table,p,playerData);
+			}else{
+				console.log('取得球员数据失败');
+			}
+		},
+		error:function(){
+			console.log('取得球员数据失败');
+		}
+	});
+}
+//初始化插入球员数据
+function initInsertPlayerData(schedule_id,table,p,playerData){
+	var sql="insert into livingroomplayerdata "+
+			"values "+
+			"("+schedule_id+",rpplayer_id,0,0,0,0,0,0,0,0,0,rpteam_id);";
+	var sqlTmp='';
+	var length=playerData.length;
+	for (var i = 0; i < length; i++) {
+		sqlTmp=sql.replace('rpplayer_id',playerData[i]['player_id'])
+				  .replace('rpteam_id',playerData[i]['team_id']);
+		$.ajax({
+			type:'GET',
+			url:"../../php/noReturnQuery.php",
+			data:{
+				sql:sqlTmp
+			},
+			async:false,
+			success:function(data){
+				if(data == 1){
+					console.log(i+'数据初始化成功，即将重新进入直播间');
+					if(i==(length-1)){
+						enterLiving(schedule_id,table,p);
+					}
+				}else{
+					console.log('初始化插入球员数据失败');
+				}
+			},
+			error:function(){
+				console.log('初始化插入球员数据连接后台失败');
+			}
+		});
+	}
 }
 //初始化比分Box
 function initScoreBox(data){
@@ -565,7 +669,7 @@ function initWordBox(page){
 	}
 	current_page=page;
 	var sql="select count(*) "+
-			"from livingroomWord "+
+			"from livingroomword "+
 			"where schedule_id="+current_schedule_id+";";
 	$.ajax({
 		type:'GET',
@@ -595,7 +699,7 @@ function paging(page){
 		page=current_page;
 	}
 	var sql="select * "+
-			"from livingroomWord "+
+			"from livingroomword "+
 			"where schedule_id="+current_schedule_id+" "+
 			"order by ms desc "+
 			"limit "+(page-1)*per_page+","+per_page+";";
@@ -719,7 +823,7 @@ function publishWord(){
 	guest_score=parseInt(guest_score);
 	home_score=parseInt(home_score);
 
-	var sql="insert into livingroomWord "+
+	var sql="insert into livingroomword "+
 			"values "+
 			"("+current_schedule_id+",'"+word+"',"+guest_score+","+home_score+",'"+ms+"');";
 	$.ajax({
@@ -741,7 +845,7 @@ function publishWord(){
 //删除直播文字
 function deleteThisWord(ms){
 	var sql="delete "+
-			"from livingroomWord "+
+			"from livingroomword "+
 			"where schedule_id="+current_schedule_id+" "+
 			"and ms='"+ms+"';";
 		$.ajax({
@@ -767,9 +871,261 @@ function deleteThisWord(ms){
 function initDataBox(){
 	$('#guestLabel').text(current_guest_name);
 	$('#homeLabel').text(current_home_name);
+
+	var sql="select livingroomplayerdata.*,player.player_name "+
+			"from livingroomplayerdata,player "+
+			"where schedule_id="+current_schedule_id+" "+
+			"and livingroomplayerdata.player_id=player.player_id;";
+	$.ajax({
+		type:'GET',
+		url:"../../php/hasReturnQuery.php",
+		data:{
+			sql:sql
+		},
+		dataType:'json',
+		success:function(data){
+			if (data != '') {
+				var minuDataButton='<button type="button" class="dataButton" onclick="minuData(this);">-</button>';
+				var addDataButton='<button type="button" class="dataButton" onclick="addData(this);">+</button>';
+				var onkeypressHTML='onkeypress="return isNum(event)"';
+				var oninputHTML='oninput="updateData(this,this.value)"';
+				var trHTML='<tr>'+
+							'<td class="player_id hidden">rpplayer_id</td>'+
+							'<td class="player_name">rpplayer_name</td>'+
+							'<td class="score">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt scoreInput" '+oninputHTML+' '+onkeypressHTML+' value="rpscore" key="score" />'+
+								addDataButton+
+							'</td>'+
+							'<td class="backboard">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt backboardInput" '+oninputHTML+' '+onkeypressHTML+' value="rpbackboard" key="backboard" />'+
+								addDataButton+
+							'</td>'+
+							'<td class="assists">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt assistsInput" '+oninputHTML+' '+onkeypressHTML+' value="rpassists" key="assists" />'+
+								addDataButton+
+							'</td>'+
+							'<td class="steal">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt stealInput" '+oninputHTML+' '+onkeypressHTML+' value="rpsteal" key="steal" />'+
+								addDataButton+
+							'</td>'+
+							'<td class="block">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt blockInput" '+oninputHTML+' '+onkeypressHTML+' value="rpblock" key="block" />'+
+								addDataButton+
+							'</td>'+
+							'<td class="error">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt errorInput" '+oninputHTML+' '+onkeypressHTML+' value="rperror" key="error" />'+
+								addDataButton+
+							'</td>'+
+							'<td class="3point">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt 3pointInput" '+oninputHTML+' '+onkeypressHTML+' value="rp3point" key="3point" />'+
+								addDataButton+
+							'</td>'+
+							'<td class="foul">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt foulInput" '+oninputHTML+' '+onkeypressHTML+' value="rpfoul" key="foul" />'+
+								addDataButton+
+							'</td>'+
+							'<td class="free_throw">'+
+								minuDataButton+
+								'<input type="text" class="dataIpunt free_throwInput" '+oninputHTML+' '+onkeypressHTML+' value="rpfree_throw" key="free_throw" />'+
+								addDataButton+
+							'</td>'
+						   '</tr>';
+				var trHTMLTmp='';
+				var $guestDataTable=$('#guestDataTable');
+				var $homeDataTable=$('#homeDataTable');
+				$guestDataTable.find('tr:not(.tableHeader)').html('');
+				$homeDataTable.find('tr:not(.tableHeader)').html('');
+
+				var length=data.length;
+				for(var i=0;i<length;i++){
+					trHTMLTmp=trHTML.replace('rpplayer_id',data[i]['player_id'])
+									.replace('rpplayer_name',data[i]['player_name'])
+									.replace('rpscore',data[i]['score'])
+									.replace('rpbackboard',data[i]['backboard'])
+									.replace('rpassists',data[i]['assists'])
+									.replace('rpsteal',data[i]['steal'])
+									.replace('rpblock',data[i]['block'])
+									.replace('rperror',data[i]['error'])
+									.replace('rp3point',data[i]['3point'])
+									.replace('rpfoul',data[i]['foul'])
+									.replace('rpfree_throw',data[i]['free_throw']);
+					if(data[i]['team_id'] == current_guest_id){
+						$guestDataTable.append(trHTMLTmp);
+					}else if(data[i]['team_id'] == current_home_id){
+						$homeDataTable.append(trHTMLTmp);
+					}
+				}
+			}
+		},
+		error:function(){
+			console.log('连接后台错误');
+		}
+	});
 }
+//输入数字，更新数据库数据
+function updateData(input,value){
+	var $tr=$(input).parent().parent();
+	var key=$(input).attr('key');
+	var player_id=$tr.find('.player_id').text();
+	var sql="update livingroomplayerdata "+
+			"set "+key+"="+value+" "+
+			"where schedule_id="+current_schedule_id+" "+
+			"and player_id="+player_id+";";
+	$.ajax({
+		type:'GET',
+		url:"../../php/noReturnQuery.php",
+		data:{
+			sql:sql
+		},
+		success:function(data){
+			if(data == 1){
+				console.log('成功将'+key+'设置为'+value);
+			}
+		},
+		error:function(){
+			console.log('连接后台错误');
+		}
+	});
+}
+//数据减1
+function minuData(button){
+	var $tr=$(button).parent().parent();
+	var value=$(button).siblings('input').val();
+	var key=$(button).siblings('input').attr('key');
+	var player_id=$tr.find('.player_id').text();
+	value=parseInt(value);
+	if(isNaN(value))return false;
+	if(value <= 0)return false;
+	value--;
 
+	var sql="update livingroomplayerdata "+
+			"set "+key+"="+value+" "+
+			"where schedule_id="+current_schedule_id+" "+
+			"and player_id="+player_id+";";
+	$.ajax({
+		type:'GET',
+		url:"../../php/noReturnQuery.php",
+		data:{
+			sql:sql
+		},
+		success:function(data){
+			if(data == 1){
+				console.log('成功将'+key+'设置为'+value);
+				$(button).siblings('input').val(value);
+			}
+		},
+		error:function(){
+			console.log('连接后台错误');
+		}
+	});
+}
+//数据加1
+function addData(button){
+	var $tr=$(button).parent().parent();
+	var value=$(button).siblings('input').val();
+	var key=$(button).siblings('input').attr('key');
+	var player_id=$tr.find('.player_id').text();
+	value=parseInt(value);
+	if(isNaN(value))return false;
+	value++;
 
+	var sql="update livingroomplayerdata "+
+			"set "+key+"="+value+" "+
+			"where schedule_id="+current_schedule_id+" "+
+			"and player_id="+player_id+";";
+	$.ajax({
+		type:'GET',
+		url:"../../php/noReturnQuery.php",
+		data:{
+			sql:sql
+		},
+		success:function(data){
+			if(data == 1){
+				console.log('成功将'+key+'设置为'+value);
+				$(button).siblings('input').val(value);
+			}
+		},
+		error:function(){
+			console.log('连接后台错误');
+		}
+	});
+}
+//结束本场比赛
+function endThisGame(schedule_id,table,button){
+	var sql="update "+table+" "+
+			"set end_flag=2 "+
+			"where schedule_id="+schedule_id+";";
+	$.ajax({
+		type:'GET',
+		url:"../../php/noReturnQuery.php",
+		data:{
+			sql:sql
+		},
+		success:function(data){
+			if (data == 1) {
+				var $p=$(button).parent();
+				uploadPlayerData(schedule_id,table,$p);
+			}else{
+				console.log('结束本场比赛失败');
+			}
+		},
+		error:function(){
+			console.log('结束本场比赛失败');
+		}
+	});
+}
+//比赛结束，上传球员数据
+function uploadPlayerData(schedule_id,table,$p){
+	var sql="update statistics st,livingroomplayerdata pd,"+table+" li "+
+			"set "+
+			"st.score=st.score+pd.score,"+
+			"st.backboard=st.backboard+pd.backboard,"+
+			"st.assists=st.assists+pd.assists,"+
+			"st.steal=st.steal+pd.steal,"+
+			"st.block=st.block+pd.block,"+
+			"st.error=st.error+pd.error,"+
+			"st.3point=st.3point+pd.3point,"+
+			"st.foul=st.foul+pd.foul,"+
+			"st.free_throw=st.free_throw+pd.free_throw,"+
+			"st.played_game=st.played_game+1,"+
+			"li.insert_flag=1 "+
+			"where pd.schedule_id="+schedule_id+" "+
+			"and st.player_id=pd.player_id "+
+			"and li.insert_flag!=1;";
+	console.log(sql);
+	$.ajax({
+		type:'GET',
+		url:"../../php/noReturnQuery.php",
+		data:{
+			sql:sql
+		},
+		success:function(data){
+			if (data == 1) {
+				console.log('比赛结束，上传球员数据成功');
+				$p.siblings('.status').text('已结束');
+				$p.remove();
+			}else{
+				console.log('比赛结束，上传球员数据'+data);
+			}
+		},
+		error:function(){
+			console.log('比赛结束，上传球员数据连接后台失败');
+		}
+	});
+}
+//退出直播间
+function exit(){
+	sessionStorage.clear();
+	location.href='login/';
+}
 
 //返回剩余时间
 function getRemainTime(todayMs,ms){
